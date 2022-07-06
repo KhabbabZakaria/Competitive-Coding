f473768e96eb1f77cb0865a169fcb370a9d30279
Khabbab Zakaria <46716277+KhabbabZakaria@users.noreply.github.com>
2022-07-06 14:16:02 +0200

    Update calculate_ways_with_dp.py

--- calculate_ways_with_dp.py
+++ calculate_ways_with_dp.py
@@ -38,3 +38,26 @@ def numberOfWaysToMakeChange(n, denoms):
             if denom<=amount:
                 dp[amount]+=dp[amount-denom]
     return dp[-1]
+
+
+
+#bottom up
+#coin problem
+#order important
+#example: target amount = 10; coin denominations = [1,5,10,25]. ways= 1coin x 10times, 5coins x 2times,
+#(1coin x 5times + 5coin x 1times), 10coin x 1times. But (5coin x 1times + 1coin x 5times) is not an option 
+# as (1coin x 5times + 5coin x 1times) is already there.
+def numberOfWaysToMakeChange(n, denoms):
+    # Write your code here.
+    #pass
+    denoms.sort()
+    dp = [0]*(n+1)
+    dp[-1] = 1
+    
+    for denom in denoms:
+        for amount in reversed(range(len(dp))):
+            if denom<=amount:
+                #dp[stair]+=dp[stair+jump-1]
+                dp[amount-denom]+=dp[amount]
+
+    return dp[0]
