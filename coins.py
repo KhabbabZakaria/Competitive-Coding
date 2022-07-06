7c47c9caee437b070c10e70611a4dcad2f916779
Khabbab Zakaria <46716277+KhabbabZakaria@users.noreply.github.com>
2022-07-06 23:35:28 +0200

    Update coins.py
    previous  solution was not an optimum one

--- coins.py
+++ coins.py
@@ -1,21 +1,11 @@
-array = [7,5,1]
-target = 18
-
-result = []
-mincount = float('inf')
-count = 1
-def coins(array, target, count):
-    global mincount
-    if target ==0:
-        return 
-    for i in array:
-        diff = target - i
-        if diff>=0:
-            count+=1
-            coins(array, diff, count)
-    if count<mincount:
-        mincount=count
-    count=1
-
-coins(array, target, count)
-print(mincount)
+def minNumberOfCoinsForChange(n, denoms):
+    dp = [float('inf')]*(n+1)
+    dp[0] = 0 # min way to make 0 dollars = 0
+    for denom in denoms:
+        for amount in range(n+1):
+            if denom<=amount:
+                diff = amount - denom
+                dp[amount] = min(dp[amount], 1 + dp[diff])
+    if dp[-1]==float('inf'):
+        dp[-1] = -1
+    return dp[-1]
